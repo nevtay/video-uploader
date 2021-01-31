f06243f1c2aa498f788198d6128cef5142ae9c7d
Nevin Tay <nevintay@gmail.com>
2021-01-31 21:47:55 +0800

    Import props, and set button components and their logic inside here

--- client/src/pages/PageTwo.js
+++ client/src/pages/PageTwo.js
@@ -1,6 +1,26 @@
 import React from "react";
+import NextButton from "../components/NextButton";
+import PrevButton from "../components/PrevButton";
 
-const PageTwo = () => {
+const PageTwo = ({ page, handlePage }) => {
+  // move forward one page
+  const toNextPage = (e) => {
+    // e.preventDefault();
+    if (page === 3) {
+      return;
+    } else {
+      handlePage((page) => page + 1);
+    }
+  };
+
+  // move back one page
+  const toPrevPage = (e) => {
+    // e.preventDefault();
+    if (page === 1) return;
+    handlePage((page) => {
+      return (page = page - 1);
+    });
+  };
   return (
     <>
       <section>
@@ -31,6 +51,8 @@ const PageTwo = () => {
         </p>
       </section>
       <input type="checkbox"></input>
+      <NextButton handleOnClick={toNextPage} page={page} />
+      <PrevButton handleOnClick={toPrevPage} page={page} />
     </>
   );
 };
