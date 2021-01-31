f60ab72ff9e59a31cb5ffa7fa99f8504bb8371f6
Nevin Tay <nevintay@gmail.com>
2021-01-31 21:12:35 +0800

    Add conditional rendering based on current form page

--- client/src/components/NextButton.js
+++ client/src/components/NextButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const NextButton = () => {
+const NextButton = ({ handleOnClick, page }) => {
   return (
     <>
-      <button>Next</button>
+      {page === 3 && (
+        <button disabled onClick={handleOnClick}>
+          Next
+        </button>
+      )}
+      {page !== 3 && <button onClick={handleOnClick}>Next</button>}
     </>
   );
 };
