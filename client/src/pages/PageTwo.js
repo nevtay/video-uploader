09a46b629cb79a6555c0d618301fac34e7b6f6b3
Nevin Tay <nevintay@gmail.com>
2021-01-31 20:27:36 +0800

    Add example T&C text and acknowledgement checkbox

---
+++ client/src/pages/PageTwo.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const PageTwo = () => {
+  return (
+    <>
+      <secton>
+        <h2>TERMS AND CONDITIONS</h2>
+        <div>
+          <strong>Application of these terms 1. </strong>
+          <p>
+            These terms and conditions shall apply in respect of all goods and
+            services we supply to you whether specified overleaf or otherwise
+            (“the Service”). Except to the extent that these terms and
+            conditions are varied by mutual consent they together with the brief
+            and quotation overleaf shall constitute the entire agreement between
+            us and shall prevail over your own terms and conditions. You agree
+            that in entering into this agreement, you shall not rely on any
+            representation or understanding, whether oral or in writing, which
+            is not included or referred to in these terms and conditions.{" "}
+          </p>
+
+          <strong>Delivery 2. </strong>
+          <p>
+            The Service shall be delivered to you in accordance with the brief
+            agreed at the outset of the project either through concept meetings
+            or in written proposals. 3. We shall produce one master copy of the
+            production only for your use. Should you require further copies an
+            additional charge will apply. A separate quotation will be issued
+            for the production of additional copies. 4. Times given for delivery
+            of the Service are estimates only and time shall not be of the
+            essence.
+          </p>
+        </div>
+      </secton>
+      <input type="checkbox">
+        I agree to the terms and condition listed above
+      </input>
+    </>
+  );
+};
+
+export default PageTwo;
