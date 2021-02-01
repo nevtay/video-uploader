63a95acbadba561506ba56327e8a626adbc0a37f
Nevin Tay <nevin.tay@global.ntt>
2021-02-01 13:52:38 +0800

    Layout changes and add props

--- client/src/pages/PageThree.js
+++ client/src/pages/PageThree.js
@@ -2,7 +2,14 @@ import React from "react";
 import NextButton from "../components/NextButton";
 import PrevButton from "../components/PrevButton";
 
-const PageThree = ({ page, handlePage }) => {
+const PageThree = ({
+  page,
+  fileName,
+  handlePage,
+  videoTitle,
+  videoDateTime,
+  videoLocation,
+}) => {
   // move forward one page
   const toNextPage = (e) => {
     // e.preventDefault();
@@ -23,15 +30,70 @@ const PageThree = ({ page, handlePage }) => {
   };
   return (
     <>
-      <div className="form-group">
-        <p>Review video metadata</p>
+      <h3 className="text-muted mb-4">Upload Summary</h3>
+      <label for="videoTitleSummary">File Name</label>
+      <div className="form-group mb-2">
+        <input
+          type="text"
+          name="videoTitle"
+          className="form-control-plaintext"
+          value={fileName}
+          readOnly
+          disabled
+        />
+      </div>
+      <div className="input-group mb-4">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">
+            Video Title
+          </span>
+        </div>
+        <input
+          type="text"
+          name="videoTitle"
+          id="videoTitleSummary"
+          className="form-control"
+          value={videoTitle}
+          readOnly
+          disabled
+        />
+      </div>
+      <div className="input-group mb-4">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">
+            Video Start Date/Time
+          </span>
+        </div>
+        <input
+          type="text"
+          name="videoDateTime"
+          id="videoDateTimeSummary"
+          className="form-control"
+          value={videoDateTime}
+          disabled
+        />
+      </div>
+      <div className="input-group mb-4">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="basic-addon1">
+            Video Location
+          </span>
+        </div>
+        <input
+          type="text"
+          name="videoLocation"
+          id="videoLocationSummary"
+          className="form-control"
+          value={videoLocation}
+          readOnly
+          disabled
+        />
       </div>
       <div className="form-group">
         <input type="submit" value="Upload video" />
       </div>
       <div className="btn-group mt-4">
         <PrevButton handleOnClick={toPrevPage} page={page} />
-        <NextButton handleOnClick={toNextPage} page={page} />
       </div>
     </>
   );
