d70504441e7971a281589398903ce70f4d455456
nevtay <nevintay@gmail.com>
2021-02-02 01:42:08 +0800

    Refactor code

--- client/src/components/NextButton.js
+++ client/src/components/NextButton.js
@@ -1,22 +1,11 @@
 import React from "react";
 
-const NextButton = ({ handleOnClick, page }) => {
+const NextButton = ({ handleOnClick }) => {
   return (
     <>
-      {page === 3 && (
-        <button
-          className="btn btn-outline-primary"
-          disabled
-          onClick={handleOnClick}
-        >
-          Next
-        </button>
-      )}
-      {page !== 3 && (
-        <button className="btn btn-outline-primary" onClick={handleOnClick}>
-          Next
-        </button>
-      )}
+      <button className="btn btn-outline-primary" onClick={handleOnClick}>
+        Next
+      </button>
     </>
   );
 };
