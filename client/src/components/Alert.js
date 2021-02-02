339435f658f7ab3a888aaf57395c3fdd57d3b15f
Nevin Tay <nevintay@gmail.com>
2021-02-02 20:54:47 +0800

    Initial commit

---
+++ client/src/components/Alert.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Alert = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100vw",
+        background: "gray",
+      }}
+    >
+      Success
+    </div>
+  );
+};
+
+export default Alert;
