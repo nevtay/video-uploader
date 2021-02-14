1f659b6301e70ac6bdca58c0795aa592b51856fb
Nevin Tay <nevintay@gmail.com>
2021-02-14 22:24:07 +0800

    Set each case to always return whole state as part of its own functionality

--- client/src/context/formReducer.js
+++ client/src/context/formReducer.js
@@ -10,6 +10,34 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case SET_FILENAME:
+      return {
+        ...state,
+      };
+    case SET_FILETYPE:
+      return {
+        ...state,
+      };
+    case SET_VIDEO_FILE:
+      return {
+        ...state,
+      };
+    case SET_VIDEO_TITLE:
+      return {
+        ...state,
+      };
+    case SET_VIDEO_DATE:
+      return {
+        ...state,
+      };
+    case SET_VIDEO_TIME:
+      return {
+        ...state,
+      };
+    case SET_VIDEO_LOCATION:
+      return {
+        ...state,
+      };
     default:
       return state;
   }
