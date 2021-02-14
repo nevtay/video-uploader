9cb39e42f3d4e4966c14de708855b8feeefe1293
Nevin Tay <nevintay@gmail.com>
2021-02-15 01:09:23 +0800

    Remove unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
-import { useState, useMemo, useRef, useContext } from "react";
 import MainDisplay from "./pages/MainDisplay";
-import PageOne from "./pages/PageOne";
-import PageTwo from "./pages/PageTwo";
-import PageThree from "./pages/PageThree";
 import "./styles/main.scss";
 
 import FormState from "./context/FormState";
-import FormContext from "./context/formContext";
 
 function App() {
   return (
