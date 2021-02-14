6db9173cb807637cf034ca30d5fd8cd70479917d
Nevin Tay <nevintay@gmail.com>
2021-02-14 22:19:49 +0800

    Import fromReducer and initialize useReducer with it

---
+++ client/src/context/FormState.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from "react";
+import formReducer from "./formReducer";
+import {
+  SET_FILENAME,
+  SET_FILETYPE,
+  SET_VIDEO_FILE,
+  SET_VIDEO_TITLE,
+  SET_VIDEO_DATE,
+  SET_VIDEO_TIME,
+  SET_VIDEO_LOCATION,
+} from "./types";
+
+const FormState = (props) => {
+  const initialState = {
+    fileName: "",
+    fileType: "",
+    videoFile: "",
+    videoTitle: "",
+    videoDate: "",
+    videoTime: "",
+    videoLocation: "",
+  };
+
+  const [state, dispatch] = useReducer(formReducer);
+  return <div></div>;
+};
+
+export default FormState;
