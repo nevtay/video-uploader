bfc4dd54a68e1695745a0e5f132355942a4fba61
Nevin Tay <nevintay@gmail.com>
2021-02-14 22:12:33 +0800

    Instantiate types

---
+++ client/src/context/types.js
@@ -0,0 +1,7 @@
+export const SET_FILENAME = "SET_FILENAME";
+export const SET_FILETYPE = "SET_FILE_TYPE";
+export const SET_VIDEO_FILE = "SET_VIDEO_FILE";
+export const SET_VIDEO_TITLE = "SET_VIDEO_TITLE";
+export const SET_VIDEO_DATE = "SET_VIDEO_DATE";
+export const SET_VIDEO_TIME = "SET_VIDEO_TIME";
+export const SET_VIDEO_LOCATION = "SET_VIDEO_LOCATION";
