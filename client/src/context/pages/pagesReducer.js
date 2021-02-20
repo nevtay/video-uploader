7306ca2ca3d14cadd8914ffa80767ea0720d7bc4
Nevin Tay <nevintay@gmail.com>
2021-02-20 15:19:20 +0800

    Add payload to cases that increment/decrement page count

--- client/src/context/pages/pagesReducer.js
+++ client/src/context/pages/pagesReducer.js
@@ -4,10 +4,12 @@ export default (state, action) => {
   switch (action.type) {
     case TO_NEXT_PAGE:
       return {
+        currentPage: action.payload,
         ...state,
       };
     case TO_PREV_PAGE:
       return {
+        currentPage: action.payload,
         ...state,
       };
     default:
