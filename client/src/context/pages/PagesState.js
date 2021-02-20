a958db514bb61c86881efca4bf8e2da146ac302e
Nevin Tay <nevintay@gmail.com>
2021-02-20 14:58:46 +0800

    Create state to track expected last page, create dispatch functions for incrementing and decrementing page count, add last page state and dispatch functions to value props in provider

--- client/src/context/pages/PagesState.js
+++ client/src/context/pages/PagesState.js
@@ -6,13 +6,31 @@ import { TO_NEXT_PAGE, TO_PREV_PAGE, MAX_PAGES } from "./types";
 const PagesState = (props) => {
   const initialState = {
     currentPage: "1",
+    lastPage: MAX_PAGES,
   };
   const [state, dispatch] = useReducer(PagesReducer, initialState);
+
+  const toNextPage = () => {
+    dispatch({
+      type: TO_NEXT_PAGE,
+      payload: state.currentPage++,
+    });
+  };
+
+  const toPrevPage = () => {
+    dispatch({
+      type: TO_PREV_PAGE,
+      payload: state.currentPage--,
+    });
+  };
 
   return (
     <PagesContext.Provider
       value={{
         currentPage: state.currentPage,
+        lastPage: state.lastPage,
+        toNextPage,
+        toPrevPage,
       }}
     >
       {props.children}
