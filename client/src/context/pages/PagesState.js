fd081ffc4dac9e49109c1c1b202345ef8c03b210
Nevin Tay <nevintay@gmail.com>
2021-02-20 14:51:47 +0800

    Create initial properties of pages state

---
+++ client/src/context/pages/PagesState.js
@@ -0,0 +1,21 @@
+import React, { useReducer, useRef } from "react";
+import PagesContext from "./pagesContext";
+import PagesReducer from "./pagesReducer";
+import { TO_NEXT_PAGE, TO_PREV_PAGE } from "./types";
+
+const PagesState = (props) => {
+  const initialState = {
+    currentPage: "1",
+  };
+  const [state, dispatch] = useReducer(PagesReducer, initialState);
+
+  return (
+    <PagesContext.Provider
+      value={{
+        currentPage: state.currentPage,
+      }}
+    >
+      {props.children}
+    </PagesContext.Provider>
+  );
+};
