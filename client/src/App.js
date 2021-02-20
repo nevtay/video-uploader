d6f8ac5d3300f93036abbc50030ecd061f4579fd
Nevin Tay <nevintay@gmail.com>
2021-02-20 15:23:45 +0800

    Import and set PagesState provider

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import MainDisplay from "./pages/MainDisplay";
 import "./styles/main.scss";
 
 import FormState from "./context/uploadFields/FormState";
+import PagesState from "./context/pages/PagesState";
 
 function App() {
   return (
     <FormState>
-      <MainDisplay />
+      <PagesState>
+        <MainDisplay />
+      </PagesState>
     </FormState>
   );
 }
